e005f7222acab176a9a0e2723fa6a52ca4870144
Lucas Neves <lzmn70@gmail.com>
2024-04-20 21:57:06 -0300

    Adição de classes para método de verificação
    Classes adicionadas para chamar e tratar o método VerificarVez2, CartasVerificacao herda de CartasChamada,  base para métodos que retornam cartas.
     Adição da classes DadosVerificacao, que possui método para tratar o retorno da verificação, contendo as informações da vez, e uma lista de CartasVerificacao, que contém os dados das cartas jogadas naquela rodada.

---
+++ MagicTrick_piIII/classes/CartasVerificacao.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicTrick_piIII.classes
+{
+    public class CartasVerificacao : CartasChamadas
+    {
+        public List<int> Valores { get; set; }    
+        public List<char> StatusCartas { get; set; }
+
+        public CartasVerificacao(int idJogador, int posicao, char naipe, int valor, char status) : base(idJogador, posicao, naipe) 
+        {
+            this.Valores = new List<int>();
+            this.StatusCartas = new List<char>();
+
+            this.Valores.Add(valor);
+            this.StatusCartas.Add(status);
+        }
+
+        public static List<CartasVerificacao> RetornarCartasVerificacaoTratadas(string[] dadosBrutos)
+        {
+            List<CartasVerificacao> cartasTmp = new List<CartasVerificacao>();
+
+            string[] dados;
+            int idJogador, valorCarta, posicaoCarta, indexJogador;
+            char naipe, status;
+
+            for(int i = 0; i < dadosBrutos.Length; i++)
+            {
+                if (dadosBrutos[i].Length == 0) continue;
+
+                if (dadosBrutos[i].StartsWith("C:"))
+                {
+                    status = 'C';
+                    dadosBrutos[i] = dadosBrutos[i].Replace("C:", "");
+                }
+                else
+                {
+                    status = 'A';
+                    dadosBrutos[i] = dadosBrutos[i].Replace("A:", "");
+                }
+
+                dados = dadosBrutos[i].Split(',');
+
+                idJogador = Convert.ToInt32(dados[0]);
+                naipe = Convert.ToChar(dados[1]);
+                valorCarta = Convert.ToInt32(dados[2]);
+
+                if (status == 'C')
+                    posicaoCarta = Convert.ToInt32(dados[3]);
+
+                else
+                    posicaoCarta = Convert.ToInt32(dados[4]);
+
+                indexJogador = cartasTmp.FindIndex(c => c.IdJogador == idJogador);
+
+                if (indexJogador > -1)
+                {
+                    cartasTmp[indexJogador].NaipeCartas.Add(naipe);
+                    cartasTmp[indexJogador].Valores.Add(valorCarta);
+                    cartasTmp[indexJogador].Posicoes.Add(posicaoCarta);
+                    cartasTmp[indexJogador].StatusCartas.Add(status);
+                }
+
+                else
+                    cartasTmp.Add(new CartasVerificacao(idJogador, posicaoCarta, naipe, valorCarta, status));
+            }
+            return cartasTmp;
+        }
+    }
+}
---
+++ MagicTrick_piIII/classes/DadosVerificacao.cs
@@ -0,0 +1,66 @@
+﻿using MagicTrickServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicTrick_piIII.classes
+{
+    public class DadosVerificacao
+    {
+        public char StatusPartida { get; set; }
+        public int IdJogador { get; set; } 
+        public int RodadaAtual { get; set; }
+        public char StatusRodada { get; set; }
+        public List<CartasVerificacao> CartasRodada { get; set; }
+
+        public DadosVerificacao(string dadosPartida) 
+        {
+            string[] dadosArray = dadosPartida.Split(',');
+
+            char status = Convert.ToChar(dadosArray[0]);
+            int idJogador = Convert.ToInt32(dadosArray[1]);
+            int rodada = Convert.ToInt32(dadosArray[2]);
+            char statusRodada = Convert.ToChar(dadosArray[3]);
+
+            this.StatusPartida = status;
+            this.IdJogador = idJogador;
+            this.RodadaAtual = rodada;
+            this.StatusRodada = statusRodada;
+            this.CartasRodada = new List<CartasVerificacao>();
+        }   
+
+        public static DadosVerificacao RetornarDadosVerificacao(int idPartida)
+        {
+            DadosVerificacao dadosVerificacao = null;
+
+            string result;
+
+            try
+            {
+                result = Jogo.VerificarVez2(idPartida);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return dadosVerificacao;
+            }
+
+            if (result.StartsWith("ERRO"))
+                return dadosVerificacao;
+
+            result = result.Replace("\r", "");
+
+            string[] dadosTmp = result.Split('\n');
+
+            dadosVerificacao = new DadosVerificacao(dadosTmp[0]);
+
+            dadosTmp = dadosTmp.Skip(1).ToArray();
+
+            dadosVerificacao.CartasRodada = CartasVerificacao.RetornarCartasVerificacaoTratadas(dadosTmp);
+
+            return dadosVerificacao;    
+        }        
+    }
+}
